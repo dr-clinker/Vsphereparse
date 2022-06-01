c3f5c1e99b0a7aaf0fd64561572aa97715d29fe6
Сергей Алёшечкин <asa-khb@mail.ru>
2022-06-01 10:05:13 +1000

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Vsphereparse
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.SizeToGB = new System.Windows.Forms.Button();
+            this.AnotherFile = new System.Windows.Forms.Button();
+            this.ExcellExport = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle3;
+            this.dataGridView1.Location = new System.Drawing.Point(25, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1117, 470);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // SizeToGB
+            // 
+            this.SizeToGB.Location = new System.Drawing.Point(1184, 12);
+            this.SizeToGB.Name = "SizeToGB";
+            this.SizeToGB.Size = new System.Drawing.Size(106, 50);
+            this.SizeToGB.TabIndex = 1;
+            this.SizeToGB.Text = "Перевести в GB";
+            this.SizeToGB.UseVisualStyleBackColor = true;
+            this.SizeToGB.Click += new System.EventHandler(this.SizeToGB_Click);
+            // 
+            // AnotherFile
+            // 
+            this.AnotherFile.Location = new System.Drawing.Point(1184, 432);
+            this.AnotherFile.Name = "AnotherFile";
+            this.AnotherFile.Size = new System.Drawing.Size(106, 50);
+            this.AnotherFile.TabIndex = 2;
+            this.AnotherFile.Text = "выбрать другой файл";
+            this.AnotherFile.UseVisualStyleBackColor = true;
+            this.AnotherFile.Click += new System.EventHandler(this.AnotherFile_Click);
+            // 
+            // ExcellExport
+            // 
+            this.ExcellExport.Location = new System.Drawing.Point(1184, 376);
+            this.ExcellExport.Name = "ExcellExport";
+            this.ExcellExport.Size = new System.Drawing.Size(106, 50);
+            this.ExcellExport.TabIndex = 3;
+            this.ExcellExport.Text = "экспорт в excel";
+            this.ExcellExport.UseVisualStyleBackColor = true;
+            this.ExcellExport.Click += new System.EventHandler(this.ExcellExport_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1302, 494);
+            this.Controls.Add(this.ExcellExport);
+            this.Controls.Add(this.AnotherFile);
+            this.Controls.Add(this.SizeToGB);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button SizeToGB;
+        private System.Windows.Forms.Button AnotherFile;
+        private System.Windows.Forms.Button ExcellExport;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Vsphereparse
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+           InitializeComponent();
+        }
+        public string Getfile()
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                //openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.Filter = "txt files (*.txt)|*.txt";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+
+                    //Read the contents of the file into a stream
+                    var fileStream = openFileDialog.OpenFile();
+
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        fileContent = reader.ReadToEnd();
+                    }
+                    return fileContent;
+                }
+                else
+                {
+                    this.Close();
+                    return "";
+                }
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string filecontent = Getfile();   
+            string[] splittedstring = filecontent.Split('\n');
+            int datastrcount = 0;
+            while(datastrcount == 0)
+            {
+                for (int i = 0; i < splittedstring.Length; i++)
+                {
+                    if (splittedstring[i] == "" || splittedstring[i] == null || splittedstring[i] == string.Empty || splittedstring[i] == "\r")
+                    {
+                        datastrcount = i;
+                        break;
+                    }
+                }
+            }
+
+            
+            int strnum = 0;
+            int columnum = 0;
+            dataGridView1.ColumnCount = datastrcount ;
+            dataGridView1.RowCount = (splittedstring.Length + datastrcount) / (datastrcount + 1);
+           //dataGridView1.RowCount = 20;
+            foreach (string line in splittedstring)
+            {
+                if (line != "\r" && line != "" && line != null && line != string.Empty)
+                {
+                    dataGridView1.Rows[strnum].Cells[columnum].Value = line;
+                    if (columnum < datastrcount - 1)
+                    {
+                        columnum++;
+                    }
+                    else
+                    {
+                        columnum = 0;
+                        strnum++;
+                    }
+                }
+                
+            }
+            
+        }
+
+        private void SizeToGB_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int columnindex = dataGridView1.CurrentCell.ColumnIndex;
+                for (int i = 0; i < dataGridView1.RowCount; i++)
+                {
+                    string value = "";
+                    string rank = "";
+                    double doubleval = 0;
+                    string strvalue = dataGridView1.Rows[i].Cells[columnindex].Value.ToString();
+                    int j = 0;
+                    while ((int.TryParse(strvalue[j].ToString(), out int a) || strvalue[j] == ',') && j < strvalue.Length)
+                    {
+                        value += strvalue[j];
+                        j++;
+                    }
+                    if (strvalue.Replace(value, "").Contains("GB"))
+                    {
+                        doubleval = double.Parse(value);
+                    }
+                    else if (strvalue.Replace(value, "").Contains("TB"))
+                    {
+                        doubleval = double.Parse(value) * 1024;
+                    }
+                    else if (strvalue.Replace(value, "").Contains("MB"))
+                    {
+                        doubleval = double.Parse(value) / 1024;
+                    }
+                    else
+                    {
+                        doubleval = double.Parse(value);
+                    }
+                    dataGridView1.Rows[i].Cells[columnindex].Value = Math.Round(doubleval, 2);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("выберите нужный столбец");
+            }
+           
+        }
+
+        private void AnotherFile_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form1 form = new Form1();
+            form.Show();
+        }
+
+        private void ExcellExport_Click(object sender, EventArgs e)
+        {
+            Microsoft.Office.Interop.Excel.Application ExcelApp = new Microsoft.Office.Interop.Excel.Application();
+            Microsoft.Office.Interop.Excel.Workbook ExcelWorkBook;
+            Microsoft.Office.Interop.Excel.Worksheet ExcelWorkSheet;
+            //Книга.
+            ExcelWorkBook = ExcelApp.Workbooks.Add(System.Reflection.Missing.Value);
+            //Таблица.
+            ExcelWorkSheet = (Microsoft.Office.Interop.Excel.Worksheet)ExcelWorkBook.Worksheets.get_Item(1);
+
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                for (int j = 0; j < dataGridView1.ColumnCount; j++)
+                {
+                    ExcelApp.Cells[i + 1, j + 1] = dataGridView1.Rows[i].Cells[j].Value;
+                }
+            }
+            //Вызываем нашу созданную эксельку.
+            ExcelApp.Visible = true;
+            ExcelApp.UserControl = true;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
